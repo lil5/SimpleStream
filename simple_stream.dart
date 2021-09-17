7cc71c828e955a31780ba5bcf9bc035f9dcdb04d
Lucian I. Last <li@last.nl>
2021-09-17 17:49:26 +0200

    Add stream

---
+++ simple_stream.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+class SimpleStream<T> {
+  late StreamController<T> _controller;
+  late Stream<T> _stream;
+  SimpleStream() {
+    _controller = StreamController<T>();
+    _stream = _controller.stream;
+  }
+
+  StreamSubscription<T> listen(
+    void Function(T)? onData, {
+    Function? onError,
+    void Function()? onDone,
+    bool? cancelOnError,
+  }) {
+    return _stream.listen(
+      onData,
+      onError: onError,
+      onDone: onDone,
+      cancelOnError: cancelOnError,
+    );
+  }
+
+  void emit(T event) {
+    _controller.add(event);
+  }
+}
